11ac1c40b28085e3a693b2d91a9557fdb44d2fbf
ZenpaiDev <ZenpaiDev@users.noreply.github.com>
2025-02-17 20:28:38 -0500

    Translated main.lua

--- client/main.lua
+++ client/main.lua
@@ -3,33 +3,33 @@ PlayerData = {}
 
 -- Functions
 local function setupMenu()
-	Wait(500)
-	PlayerData = QBCore.Functions.GetPlayerData()
-	local resources = lib.callback.await('ps-adminmenu:callback:GetResources', false)
-	local commands = lib.callback.await('ps-adminmenu:callback:GetCommands', false)
-	local items = lib.callback.await('ps-adminmenu:callback:GetItems', false)
-	local vehicles = lib.callback.await('ps-adminmenu:callback:GetVehicles', false)
-	local server = lib.callback.await('ps-adminmenu:callback:GetServerInfo', false)
-	GetData()
-	SendNUIMessage({
-		action = "setupUI",
-		data = {
-			actions = Config.Actions,
-			resources = resources,
-			playerData = PlayerData,
-			commands = commands,
-			items = items,
-			vehicles = vehicles,
-			server = server 
-		}
-	})
+    Wait(500)
+    PlayerData = QBCore.Functions.GetPlayerData()
+    local resources = lib.callback.await('ps-adminmenu:callback:GetResources', false)
+    local commands = lib.callback.await('ps-adminmenu:callback:GetCommands', false)
+    local items = lib.callback.await('ps-adminmenu:callback:GetItems', false)
+    local vehicles = lib.callback.await('ps-adminmenu:callback:GetVehicles', false)
+    local server = lib.callback.await('ps-adminmenu:callback:GetServerInfo', false)
+    GetData()
+    SendNUIMessage({
+        action = "setupUI",
+        data = {
+            actions = Config.Actions,
+            resources = resources,
+            playerData = PlayerData,
+            commands = commands,
+            items = items,
+            vehicles = vehicles,
+            server = server 
+        }
+    })
 end
 
 RegisterNUICallback('getServerInfo', function(_, cb)
     local serverInfo = lib.callback.await('ps-adminmenu:callback:GetServerInfo', false)
     if not serverInfo then
-        print("Erro: Nenhum dado recebido do servidor.")
-        cb({ error = "Erro ao carregar informações do servidor." })
+        print("Error: No data received from the server.")
+        cb({ error = "Error loading server information." })
         return
     end
     cb(serverInfo)
@@ -38,78 +38,78 @@ end)
 
 -- Event Handlers
 AddEventHandler("QBCore:Client:OnPlayerLoaded", function()
-	setupMenu()
+    setupMenu()
 end)
 
 AddEventHandler("onResourceStart", function(resourceName)
-	if (GetCurrentResourceName() == resourceName) then
-		setupMenu()
-	end
+    if (GetCurrentResourceName() == resourceName) then
+        setupMenu()
+    end
 end)
 
 -- NUICallbacks
 RegisterNUICallback("hideUI", function()
-	ToggleUI(false)
+    ToggleUI(false)
 end)
 
 RegisterNUICallback("clickButton", function(data)
-	local selectedData = data.selectedData
-	local key = data.data
-	local data = CheckDataFromKey(key)
-	if not data or not CheckPerms(data.perms) then return end
+    local selectedData = data.selectedData
+    local key = data.data
+    local data = CheckDataFromKey(key)
+    if not data or not CheckPerms(data.perms) then return end
 
-	if data.type == "client" then
-		TriggerEvent(data.event, key, selectedData)
-	elseif data.type == "server" then
-		TriggerServerEvent(data.event, key, selectedData)
-	elseif data.type == "command" then
-		ExecuteCommand(data.event)
-	end
+    if data.type == "client" then
+        TriggerEvent(data.event, key, selectedData)
+    elseif data.type == "server" then
+        TriggerServerEvent(data.event, key, selectedData)
+    elseif data.type == "command" then
+        ExecuteCommand(data.event)
+    end
 
-	Log("Action Used: " .. key,
-            PlayerData.name ..
-            " (" ..
-            PlayerData.citizenid ..
-            ") - Used: " .. key .. (selectedData and (" with args: " .. json.encode(selectedData)) or ""))
+    Log("Action Used: " .. key,
+        PlayerData.name ..
+        " (" ..
+        PlayerData.citizenid ..
+        ") - Used: " .. key .. (selectedData and (" with args: " .. json.encode(selectedData)) or ""))
 end)
 
 -- Open UI Event
 RegisterNetEvent('ps-adminmenu:client:OpenUI', function()
-	if not CheckPerms(Config.OpenPanelPerms) then return end
-	ToggleUI(true)
+    if not CheckPerms(Config.OpenPanelPerms) then return end
+    ToggleUI(true)
 end)
 
 -- Close UI Event
 RegisterNetEvent('ps-adminmenu:client:CloseUI', function()
-	ToggleUI(false)
+    ToggleUI(false)
 end)
 
 -- Change resource state
 RegisterNUICallback("setResourceState", function(data, cb)
-	local resources = lib.callback.await('ps-adminmenu:callback:ChangeResourceState', false, data)
-	cb(resources)
+    local resources = lib.callback.await('ps-adminmenu:callback:ChangeResourceState', false, data)
+    cb(resources)
 end)
 
 -- Get players
 RegisterNUICallback("getPlayers", function(data, cb)
-	local players = lib.callback.await('ps-adminmenu:callback:GetPlayers', false)
-	cb(players)
+    local players = lib.callback.await('ps-adminmenu:callback:GetPlayers', false)
+    cb(players)
 end)
 
 -- Get Groups
 RegisterNUICallback("getGroupsData", function(data, cb)
-	local groups = lib.callback.await('ps-adminmenu:callback:GetGroupsData', false)
-	cb(groups)
+    local groups = lib.callback.await('ps-adminmenu:callback:GetGroupsData', false)
+    cb(groups)
 end)
 
 -- ExecuteCommand
 RegisterNetEvent('ps-adminmenu:client:ExecuteCommand', function(data)
-	ExecuteCommand(data)
+    ExecuteCommand(data)
 end)
 
 RegisterNUICallback("executeCommand", function(data, cb)
-	local command = data.command
-	local args = data.args
-	ExecuteCommand(command, args)
-	cb(command)
+    local command = data.command
+    local args = data.args
+    ExecuteCommand(command, args)
+    cb(command)
 end)
